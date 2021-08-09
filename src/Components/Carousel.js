504c860fea458ef396909a9b8390123b22d78406
Amol-Tandon <amol.tandon@mail.vinove.com>
2021-08-09 13:42:27 +0530

    second commit

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -7,15 +7,14 @@ class Carousel extends React.Component {
         super();
         this.state = {
             info: [],
-            carousel_index: 1,
-            page_index: 0,
-            loading: false, 
+            page_index: 1,
+            loading: false,
             carousel_display: false,
             btn_display: true
         }
         console.log(this.state.info, "I'm in constructor!")
     }
-
+    
     componentDidMount() {
         console.log("App Mounted!")
         fetch(`https://picsum.photos/v2/list?page=${this.state.page_index}&limit=5`)
@@ -29,22 +28,23 @@ class Carousel extends React.Component {
     }
 
     apiCall(index) {
-        console.log(index, "in api call")
-        if (index === 4 || this.state.page_index === 0) {
-            this.state.page_index++
-            fetch(`https://picsum.photos/v2/list?page=${this.state.page_index}&limit=5`)
-                .then((response) => response.json())
-                .then((data) => this.setState({
-                    info: data
-                },
-                    () => console.log(data))
-                )
-        }
+        // console.log(index, "in api call")
+        index++
+        this.setState({
+            page_index: index
+        })
+        fetch(`https://picsum.photos/v2/list?page=${index}&limit=5`)
+            .then((response) => response.json())
+            .then((data) => this.setState({
+                info: data
+            },
+                () => console.log(data))
+            )
     }
-    mainBtn(){
+    mainBtn() {
         this.setState({
             carousel_display: true,
-            loading : false,
+            loading: false,
             btn_display: false
         })
     }
@@ -53,7 +53,7 @@ class Carousel extends React.Component {
         return (
             <div>
                 {this.state.btn_display && <Button className="start" variant="contained" color="secondary" onClick={() => this.mainBtn()}>Show Carousel</Button>}
-                
+
                 {this.state.loading && <CircularProgress color="secondary" />}
                 {this.state.carousel_display && <div className="carousel">
                     {/* Carousel */}
@@ -64,11 +64,9 @@ class Carousel extends React.Component {
                                     return (
                                         <div key={index} className={"carousel-item " + (index === 0 ? 'active' : '')}>
                                             <img className="d-block w-100" src={pic.download_url} alt="First slide" />
-                                            {this.state.carousel_index = index}
                                         </div>
                                     )
                                 })}
-                                {console.log("outside map!", this.state.carousel_index)}
                                 <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span className="sr-only">Previous</span>
@@ -77,22 +75,18 @@ class Carousel extends React.Component {
                                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span className="sr-only">Next</span>
                                 </a>
-                                {console.log(this.state.carousel_index, "outside button")}
                             </div>
                             <button
-                                variant = "contained"
-                                color = "secondary" 
+                                variant="contained"
+                                color="secondary"
                                 className="nextBtn"
-                                onClick={() => this.state.carousel_index === 4 ? this.apiCall(this.state.carousel_index) : ''}>
+                                onClick={() => this.apiCall(this.state.page_index)}>
                                 Next Page
                             </button>
-
                         </div>
                     }
                 </div>}
-
             </div>
-
         )
     }
 }
